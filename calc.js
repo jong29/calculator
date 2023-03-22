882d0af9a606dc1ceb485b7b96739dccead49cbf
jong29 <jongwoop97@gmail.com>
2023-03-23 00:17:03 +0900

    add operator onlcick

--- calc.js
+++ calc.js
@@ -30,17 +30,32 @@ function operate(operator, a, b) {
 }
 
 function display(displayValue) {
-    const disV = document.querySelector(".display")
+    const disV = document.querySelector(".display");
     disV.textContent = displayValue;
 }
 
 // Variables
-displayValue = 0;
+let displayValue;
+let op1;
+let op2;
+let operator;
 
-const numbers = document.querySelectorAll(".number")
+const numbers = document.querySelectorAll(".number");
 numbers.forEach((e) => {
     const i = e.textContent;
     e.addEventListener("click", (e) => {
         display(i);
     });
 });
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((e) => {
+    const i = e.textContent;
+    e.addEventListener("click", (e) => {
+        const op1d = document.querySelector(".display");
+        op1 = op1d.textContent;
+        operator = e.textContent;
+    });
+});
+
+const equal = document.getElementById("equals").addEventListener('click', )
